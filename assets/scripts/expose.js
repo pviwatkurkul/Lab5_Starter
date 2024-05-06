8ad307c6c249257ddd69e7ece5ab41b457091913
pviwatkurkul <pviwatkurkul@ucsd.edu>
2024-05-06 00:14:27 -0700

    added pic and audio

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,29 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+var input = document.getElementById("horn-select");
+var soundInput = document.querySelector("button");
+input.addEventListener("change", init);
+soundInput.addEventListener("click",init);
+
 function init() {
-  // TODO
+  var image = document.querySelector('img');
+  var sound = document.getElementsByClassName("hidden")[0];
+  console.log(sound);
+  if (input.value == "air-horn"){
+    image.src = "assets/images/air-horn.svg";
+    sound.src = "assets/audio/air-horn.mp3";
+    sound.play();
+  }
+  else if(input.value == "car-horn"){
+    image.src = "assets/images/car-horn.svg";
+    sound.src = "assets/audio/car-horn.mp3";
+    sound.play();
+  }
+  else if(input.value == "party-horn"){
+    image.src = "assets/images/party-horn.svg";
+    sound.src = "assets/audio/party-horn.mp3";
+    sound.play();
+  }
 }
+
