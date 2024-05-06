0b24fd2b970970316238ba9152dbbc35a7b9cedc
pviwatkurkul <pviwatkurkul@ucsd.edu>
2024-05-06 16:05:11 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,22 @@
 // explore.js
-
 window.addEventListener('DOMContentLoaded', init);
 
+
+// var but = document.querySelector("button");
+// but.addEventListener("click",init);
 function init() {
-  // TODO
+  const voices = speechSynthesis.getVoices();
+  
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    document.getElementById("voice-select").appendChild(option);
+  }
 }
