a989b7f2f6afdb5f3f375d82895cea3065e4ab36
pviwatkurkul <pviwatkurkul@ucsd.edu>
2024-05-06 16:36:32 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,22 +1,39 @@
 // explore.js
 window.addEventListener('DOMContentLoaded', init);
+const synth = window.speechSynthesis;
 
 
 // var but = document.querySelector("button");
 // but.addEventListener("click",init);
 function init() {
+  if (typeof speechSynthesis === "undefined") {
+    return;
+  }
+
   const voices = speechSynthesis.getVoices();
   
-  for (let i = 0; i < voices.length; i++) {
-    const option = document.createElement("option");
-    option.textContent = `${voices[i].name} (${voices[i].lang})`;
 
-    if (voices[i].default) {
-      option.textContent += " — DEFAULT";
+  function populateVoiceList() {
+
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      document.getElementById("voice-select").appendChild(option);
     }
+  }
 
-    option.setAttribute("data-lang", voices[i].lang);
-    option.setAttribute("data-name", voices[i].name);
-    document.getElementById("voice-select").appendChild(option);
+  populateVoiceList();
+  if (
+    typeof speechSynthesis !== "undefined" &&
+    speechSynthesis.onvoiceschanged !== undefined
+  ) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
   }
 }
