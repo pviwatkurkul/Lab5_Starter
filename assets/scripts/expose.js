5b0d3ff9cab87c02c7a6ebe3e6c31c225fdb6f47
pviwatkurkul <pviwatkurkul@ucsd.edu>
2024-05-06 15:37:12 -0700

    added confetti

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,22 +1,22 @@
 // expose.js
 
 window.addEventListener('DOMContentLoaded', init);
-
-
+window.addEventListener('DOMContentLoaded', func);
 
 var input = document.getElementById("horn-select");
-var slide = document.getElementbyId("volume");
+var sound = document.getElementsByClassName("hidden")[0];
+
+var slide = document.getElementById("volume");
+var audioImg = document.querySelectorAll('img')[1];
 var soundInput = document.querySelector("button");
-var soundControls = document.getElementsByClassName("volume-controls");
+var soundControls = document.getElementById("volume-controls");
+
 input.addEventListener("change", init);
 soundInput.addEventListener("click",init);
-soundControls.addEventListener("input",init);
+soundControls.addEventListener("input",func);
 
 function init() {
   var image = document.querySelector('img');
-  var sound = document.getElementsByClassName("hidden")[0];
-  var audioImg = document.querySelectorAll('img')[1];
-  console.log(sound);
   if (input.value == "air-horn"){
     image.src = "assets/images/air-horn.svg";
     sound.src = "assets/audio/air-horn.mp3";
@@ -30,9 +30,15 @@ function init() {
   else if(input.value == "party-horn"){
     image.src = "assets/images/party-horn.svg";
     sound.src = "assets/audio/party-horn.mp3";
+    const jsConfetti = new JSConfetti({ input })
+    jsConfetti.addConfetti()
+
     sound.play();
   }
-  
+
+}
+
+function func() {
   if(slide.value == 0 ){
     audioImg.src = "assets/icons/volume-level-0.svg";
     sound.volume = (slide.value)/100;
@@ -50,4 +56,3 @@ function init() {
     sound.volume = (slide.value)/100;
   }
 }
-
