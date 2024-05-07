171d0b680292b0f64fc3c2d61764e5aa22a6f04e
pviwatkurkul <pviwatkurkul@ucsd.edu>
2024-05-06 17:15:45 -0700

    added speech utterance and changing face

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,17 +3,18 @@ window.addEventListener('DOMContentLoaded', init);
 const synth = window.speechSynthesis;
 
 
-// var but = document.querySelector("button");
-// but.addEventListener("click",init);
+
 function init() {
+
   if (typeof speechSynthesis === "undefined") {
     return;
   }
-
-  const voices = speechSynthesis.getVoices();
   
-
+  let voices = [];
+  
   function populateVoiceList() {
+    voices = speechSynthesis.getVoices();
+
 
     for (let i = 0; i < voices.length; i++) {
       const option = document.createElement("option");
@@ -36,4 +37,29 @@ function init() {
   ) {
     speechSynthesis.onvoiceschanged = populateVoiceList;
   }
+
+  let talk = document.querySelector("button");
+  let inputTxt = document.getElementById("text-to-speak");
+  let voiceSelect = document.querySelector("select");
+  talk.addEventListener("click",function(){
+    const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+    const selectedOption =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    synth.speak(utterThis);
+
+
+    let image = document.querySelector("img");
+    if(synth.speaking){
+      image.src = "assets/images/smiling-open.png";
+    }
+    
+    utterThis.addEventListener("end",function(){
+      image.src = "assets/images/smiling.png";
+    });
+  });
 }
