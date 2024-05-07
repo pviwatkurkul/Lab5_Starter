09a14ffdd85d924a553cbd2a5332e4667a48fc7f
pviwatkurkul <pviwatkurkul@ucsd.edu>
2024-05-06 22:04:49 -0700

    added unit tests

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Lab 5 - Starter  
+Purich Viwatkurkul  
 
-Expose Page: https://pviwatkurkul.github.io/Lab5_Starter/expose.html
+Expose Page: https://pviwatkurkul.github.io/Lab5_Starter/expose.html  
 
 ## Check your Understanding
 1. We would not use a unit test to test messaging between two users because the point of a unit test is that it cannot test how indivdiual components interact with each other on a feature level.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,87 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+test('email test 1: true', () => {
+  expect(isEmail('test123@gmail.com')).toBe(true);
+});
+
+test('email test 2: true', () => {
+  expect(isEmail('testmail@gmail.com')).toBe(true);
+});
+
+test('email test 3: false', () => {
+  expect(isEmail('#23fga@gmail.com')).toBe(false);
+});
+
+test('email test 4: false', () => {
+  expect(isEmail('5452353a@.com')).toBe(false);
+});
+
+
+test('phonenumber test 1: true', () => {
+  expect(isPhoneNumber("523-412-5161")).toBe(true);
+});
+
+test('phonenumber test 2: true', () => {
+  expect(isPhoneNumber("555-123-4567")).toBe(true);
+});
+
+
+test('phonenumber test 3: false', () => {
+  expect(isPhoneNumber("523-(412)-5161")).toBe(false);
+});
+
+test('phonenumber test 4: false', () => {
+  expect(isPhoneNumber("12341411")).toBe(false);
+});
+
+test('strongPassword test 1: true', () => {
+  expect(isStrongPassword("aBcd123456789")).toBe(true);
+});
+
+test('strongPassword test 2: true', () => {
+  expect(isStrongPassword("Hello1234")).toBe(true);
+});
+
+
+test('strongPassword test 3: false', () => {
+  expect(isPhoneNumber("12345")).toBe(false);
+});
+
+test('strongPassword test 4: false', () => {
+  expect(isPhoneNumber("password")).toBe(false);
+});
+
+test('isDate test 1: true', () => {
+  expect(isDate("05/06/2024")).toBe(true);
+});
+
+test('isDate test 2: true', () => {
+  expect(isDate("12/31/2025")).toBe(true);
+});
+
+
+test('isDate test 3: false', () => {
+  expect(isDate("5/6/24")).toBe(false);
+});
+
+test('isDate test 4: false', () => {
+  expect(isDate("3/40/223")).toBe(false);
+});
+
+test('isHexColor test 1: true', () => {
+  expect(isHexColor("3F4a7d")).toBe(true);
+});
+
+test('isHexColor test 2: true', () => {
+  expect(isHexColor("#FFF")).toBe(true);
+});
+
+
+test('isHexColor test 3: false', () => {
+  expect(isHexColor("#G12E4F")).toBe(false);
+});
+
+test('isHexColor test 4: false', () => {
+  expect(isHexColor("#12345")).toBe(false);
+});
